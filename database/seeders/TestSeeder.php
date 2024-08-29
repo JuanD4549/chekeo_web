1d7372d80f946bbbf5f796056bba867d1909f600
Juan Diego <juandiego402@gmail.com>
2024-08-29 08:14:50 -0500

    update seeder

--- database/seeders/TestSeeder.php
+++ database/seeders/TestSeeder.php
@@ -100,27 +100,53 @@ class TestSeeder extends Seeder
             'user_id' => 4,
             'name' => 'Mensajeria',
         ]);
+        //Places
+
+        DB::table('places')->insert([
+            'branche_id'=>1,
+            'nane'=>'Puesta Principal',
+            'type'=>'false',
+        ]);
+        DB::table('places')->insert([
+            'branche_id'=>1,
+            'nane'=>'Puesta Secundaria',
+            'type'=>'false',
+        ]);
         //Calendar
         //1
         DB::table('calendars')->insert([
-            'name'=>'Calendar',
-            'monday' => json_encode(['time_in' => '08:30',
-                'time_out' => '17:30']),
-            'tuesday' => json_encode(['time_in' => '08:30',
-                'time_out' => '17:30']),
-            'wednesday' => json_encode(['time_in' => '08:30',
-                'time_out' => '17:30']),
-            'thursday' => json_encode(['time_in' => '08:30',
-                'time_out' => '17:30']),
-            'friday' => json_encode(['time_in' => '08:30',
-                'time_out' => '17:30']),
-            'saturday' => json_encode(['time_in' => '08:30',
-                'time_out' => '17:30']),
-            'sunday' => json_encode(['time_in' => '08:30',
-                'time_out' => '17:30']),
+            'name' => 'Calendar',
+            'monday' => json_encode([
+                'time_in' => '08:30',
+                'time_out' => '17:30'
+            ]),
+            'tuesday' => json_encode([
+                'time_in' => '08:30',
+                'time_out' => '17:30'
+            ]),
+            'wednesday' => json_encode([
+                'time_in' => '08:30',
+                'time_out' => '17:30'
+            ]),
+            'thursday' => json_encode([
+                'time_in' => '08:30',
+                'time_out' => '17:30'
+            ]),
+            'friday' => json_encode([
+                'time_in' => '08:30',
+                'time_out' => '17:30'
+            ]),
+            'saturday' => json_encode([
+                'time_in' => '08:30',
+                'time_out' => '17:30'
+            ]),
+            'sunday' => json_encode([
+                'time_in' => '08:30',
+                'time_out' => '17:30'
+            ]),
 
         ]);
-        
+
         // DB::table('user')->where('id', 3)->update([
         //     'branche_id' => 1
         // ]);
